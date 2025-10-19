e4912f0e9f0af18a6b2bd5bdd1ea2ee50522da47
Emam saimon <likonsaimon123@gmail.com>
2025-10-19 12:16:03 +0600

    ok

--- p2.cpp
+++ p2.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-  int n;
+  int n , i;
   cout << "input a Number:" << endl;
   cin >> n;
-  for (int i = 0; i<=n, i++){
+  for ( i = 0; i<=n, i++){
     cout << "Hello" << endl;
   }
 
